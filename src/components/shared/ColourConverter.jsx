7c277f971905135842ddde18d700956d89c9ba6e
TY0018 <111295767+TY0018@users.noreply.github.com>
2023-10-27 23:15:33 +0800

    fix: color converter to 3 mths

--- src/components/shared/ColourConverter.jsx
+++ src/components/shared/ColourConverter.jsx
@@ -1,12 +1,12 @@
 export default function colourConverter(timeDifference) {
   // Calculate the time thresholds in milliseconds
-  const oneMonthInMillis = 30 * 24 * 60 * 60 * 1000; // 30 days
+  const threeMonthInMillis = 30 * 24 * 60 * 60 * 1000 * 3; // 30 days
 
   if (timeDifference < 0) {
     // If the provided date is in the past
     return '#ff8787'; // Expired (in red)
   }
-  if (timeDifference <= oneMonthInMillis) {
+  if (timeDifference <= threeMonthInMillis) {
     return '#ffd43b'; // Less than 1 month away (in yellow)
   }
   return '#69db7c'; // More than 6 months away (in green)
