4c5ae13e6c60c8d2820ee49f2b9943502e79f660
Ryan Lau <lauquanfengryan@gmail.com>
2023-10-31 17:24:26 +0800

    feat: added application context to chatbot

--- src/components/chatbot/pages/chatbot.jsx
+++ src/components/chatbot/pages/chatbot.jsx
@@ -23,9 +23,63 @@ import { useNavigate } from 'react-router-dom';
 const API_KEY = import.meta.env.VITE_OPENAI_API_KEY;
 const systemMessage = {
   role: 'system',
-  // sorry for long prompt, but json was not working :(
   content:
-    "I want you to take on the persona of Oscar the owl, who is a chatbot for a RentSG app. This app serves as a tool through which foreign tenants' passes can be verified for their validity, ensuring compliance from tenants and ultimately preventing unintentional harboring. Initially, landlords would have to verify through multiple websites like the Ministry of Manpower (MOM) and Immigration and Checkpoints Authority (ICA) websites, but this helps by streamlining user workflow by having it in one platform. You are likely to get asked questions about the various types of MOM/ICA passes, tenancy documents, and the workflow of the provided application. You will hence primarily handle these questions. Make your responses very concise, non-repetitive and easy to follow. If you are unable to answer a particular question accurately, direct users to support with the following links:  https://www.ica.gov.sg/faqs/ica-faqs, support: https://www.mom.gov.sg/contact-us. However, bear in mind that I want you to give responses mostly; only provide links if you really do not know the answer.",
+    "Act as a chatbot for a web application named 'RentSG'. You only need to navigate to this website to use the app. You will take on the persona of Oscar the Owl. You will be asked questions about the various types of Singapore's Minitry of Manpower (MOM) / Immigration Checkpoint Authority (ICA) passes, tenancy documents, and the workflow of the provided application. Make your responses CONCISE, NON-REPETITVE and EASY-TO-FOLLOW. If you are unable to answer a particular question confidently or the questions are irrelevant, direct users to support with the following links:  https://www.ica.gov.sg/faqs/ica-faqs, support: https://www.mom.gov.sg/contact-us. However, I want you to give responses mostly; only provide links if you really do not know the answer.",
+};
+
+const applicationStructPrompt = `{
+  "appStructure": {
+    "ownerPage": {
+      "title": "Homeowner View",
+      "description": "Home view for homeowners when they log in. It provides an overview of which tenants are registered to the tenant residences. On the bottom navigation bar, they can toggle between home page and profile page. There is a chatbot access button on the bottom right corner.",
+      "functions": {
+        "owner_profile": {
+          "title": "Owner Profile",
+          "description": "Located in home owner profile page. Contains owner details including: Name, NRIC, Next-of-kin (NOK) name and contact information of NOK. It will also display the details of the owner residences, type of residence and number of rooms available to rent."
+        },
+        "tenant_profile": {
+          "title": "Tenant Profile",
+          "description": "Located in owner home page. Contains tenant details including: Name, Masked FIN number, pass expiry, lease expiry and contact number. It also contains a 2 buttons at the bottom which allows owners to delete tenants and view their tenancy contract agreements."
+        },
+        "add_tenant": {
+          "title": "Add Tenant",
+          "description": "Located in owner home page. THIS is a KEY feature of the application. Owner must ask tenants to navigate to their profile pages and then scan the QR code. After scanning their QR code, owners will have to carry out facial scanning of tenants to verify them and their details. They will then be required to verify tenant details approve the tenant. Once approved, the tenant will be registered to the owner residence."
+        },
+        "chatbot_function": {
+          "title": "Chatbot Function",
+          "description": "Located in chatbot page. Provides users access to the chatbot to ask questions regarding the application."
+        }
+      }
+    },
+    "tenantPage": {
+      "title": "Tenant View",
+      "description": "View for tenant when they log into the application. On the bottom navigation bar, they can toggle between home page and profile page. There is a chatbot access button on the bottom right corner.",
+      "functions": {
+        "tenant_profile": {
+          "title": "Tenant Profile",
+          "description": "Located in tenant profile page. Contains tenant personal details including: Name, FIN Number, Face image, Nationality, Pass Expiry, Contact Number. MOST IMPORTANTLY, it also has a QR code which is used in the process for homeowner to register tenants to their residences."
+        },
+        "pass_status": {
+          "title": "Pass Status",
+          "description": "Located in top of tenant home page. Indicates whether their work pass is still valid. It will be green if valid and red if not."
+        },
+        "residence_status": {
+          "title": "Residence Status",
+          "description": "Located in tenant home page. Displays information of which residence tenants belong to. It shows the residence address, the end of lease date and number of days left for the tenant to stay in the residence."
+        },
+        "chatbot_function": {
+          "title": "Chatbot Function",
+          "description": "Located in chatbot page. Provides users access to the chatbot to ask questions regarding the application."
+        }
+      }
+    }
+  }
+}
+`;
+
+const assistantMessage = {
+  role: 'assistant',
+  content: `This app serves as a tool through which foreign tenants' passes can be verified for their validity, ensuring compliance from tenants and ultimately preventing unintentional harboring by streamlining user workflow by having it in one platform. Here is information on the application structure in a JSON format: ${applicationStructPrompt}.`,
 };
 
 function ChatBotPage({ name }) {
@@ -76,7 +130,7 @@ function ChatBotPage({ name }) {
     // determine how we want chatGPT to act.
     const apiRequestBody = {
       model: 'gpt-3.5-turbo',
-      messages: [systemMessage, ...apiMessages],
+      messages: [systemMessage, assistantMessage, ...apiMessages],
     };
 
     await fetch('https://api.openai.com/v1/chat/completions', {
